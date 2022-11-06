81beabf27bb6c2d5e5e3552f26d728cef1801060
Merildor <merildor.webdeveloper@gmail.com>
2022-11-06 14:27:43 +0100

    Almost done

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 
+    'https://omnivorescookbook.com/wp-content/uploads/2022/10/220810_Tomato-Egg-Stir-Fry_3.jpg'),
+    new Recipe('Second Test Recipe', 'This is small description', 
+    'https://runningonrealfood.com/wp-content/uploads/2021/02/Vegan-One-Pot-Pasta-Recipe.jpg')
+  ];
 
   constructor() { }
 
